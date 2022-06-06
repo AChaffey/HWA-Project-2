849b7a4cfdf8f5c642824737fe0e144b1fed23d5
ajpch <ajpchaffey@hotmail.com>
2022-06-06 16:31:30 +0100

    built prackage

--- project2/src/main/java/com/qa/project2/Project2Application.java
+++ project2/src/main/java/com/qa/project2/Project2Application.java
@@ -1,4 +1,4 @@
-package src.main.java.com.qa.project2;
+package com.qa.project2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
